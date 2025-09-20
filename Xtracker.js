838afa19d55741ee7a3a81756813a78a244a5277
Big <okoemu4@gmail.com>
2025-09-20 02:32:32 +0100

    2am

--- Xtracker.js
+++ Xtracker.js
@@ -36,24 +36,49 @@ let expenses =[];
 //function...
 
 //function to add an expense
-function addExpense (name, amount) {
-      let expense = {name: name,
-            amount: amount
+function addExpense (name, amount, catergory) {
+     let name = 
+     document.getElementById("expenseName").value;
+
+     let amount = parseFloat(document.getElementById("expenseAmount")).value;
+
+     let catergory = 
+     document.getElementById("expenseCatergory").value;
+
+     if (!name || isNaN(amount)) {
+        alert("Please enter a valid name and amount");
+        return;
+     }
+
+         let expense = {name: name,
+            amount: amount,
+            category: catergory,
+            date: new Date().toLocaleDateString()
       };
 
-expenses.push(expense);
+expenses.push(expense);   //save into the list 
 
-renderExpenses();
-updateTotal();
+renderExpenses();    //update the list display
+updateTotal();         //update total
+
+// clear inputs
+
+document.getElementById("expenseName").value = "";
+document.getElementById("expenseAmount").value = "";
+document.getElementById("expenseCatergory").value = "Food";
+
 }
 
 //function to display all expenses
+
 function renderExpenses(){
-     expenseList.innerHTML = ""; 
-     expenses.forEach(exp => {
-      let li = document.createElement(li);
-      li.textContent = `${exp.name}: $
-      {exp.amount}`;
+      let expenseList = document.getElementById("expenseList");
+     expenseList.innerHTML = "";   //clear old list
+
+     expenses.forEach((expense, index) => {
+      let li = document.createElement("li");
+      li.textContent = `${expense.name} - $$
+      {expense.amount} [${expense.category}] (on ${expense.date})`;
          expenseList.appendChild(li);
      });
 }
@@ -61,8 +86,8 @@ function renderExpenses(){
 // function to calculate the total
 
 function updateTotal() {
-      let total = expenses.reduce((sum, exp) => sum + exp.amount, 0);
-      totalDisplay.textContent = total;
+      let total = expenses.reduce((sum, expense) => sum + expense.amount, 0);
+      document.getElementById("totalAmount").textContent = total;
 }
 
 
@@ -70,3 +95,20 @@ function updateTotal() {
 //addExpense → takes the data from your input boxes and stores it.
 //renderExpenses → shows everything in the list on screen.
 //updateTotal → calculates and updates the total at the bottom.
+
+
+// filter expenses by catergory 
+
+function filterExpenses (){
+      let selectedCatergory = 
+      document.getElementById("filterCatergory").value;
+
+      if (selectedCatergory === "All") {
+            renderExpenses(expenses);
+            
+      } else {
+            let filtered = expenses.filter(expense => expense.catergory === selectedCatergory);          
+            renderExpenses(filtered);
+      }
+}
+
